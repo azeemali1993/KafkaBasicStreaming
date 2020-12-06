10bbeddae347f2fa870ebf366be6708d50a976bf
azeemali1993 <azeemali.1993@gmail.com>
2020-12-06 02:52:18 -0500

    ConsumerDemo containes an active Consumer in while loop listening to first_topic.

--- src/main/java/org/azali/kafka/ConsumerDemo.java
+++ src/main/java/org/azali/kafka/ConsumerDemo.java
@@ -1,10 +1,17 @@
 package org.azali.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 
@@ -12,13 +19,36 @@ public class ConsumerDemo {
     public static void main(String[] args) {
         final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
         String BootstrapServer = "127.0.0.1:9092";
+        String groupID = "my_fourth_app";
 
         //creating consumer config
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,BootstrapServer );
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
 
+        //subscribe a consumer to the topic
+        consumer.subscribe(Collections.singleton("first_topic"));
+
+        //polling data
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Received new metadata \n" +
+                        "Key: " + record.value() + " \n" +
+                        "Partition: " + record.partition() + "\n" +
+                        "Offset: " + record.offset() + "\n" + "\n\n");
+            }
+        }
     }
+
+
 }
